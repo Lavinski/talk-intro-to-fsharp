7cea948377d3a128448149d8f97fdbe5c7c744ef
Daniel Little <lavinski@live.com.au>
2017-02-18 21:50:23 +1000

    Added Notifications and Twitter search

--- src/TwitterAlerts/TwitterAlerts.fs
+++ src/TwitterAlerts/TwitterAlerts.fs
@@ -1,107 +1,119 @@
-module TwitterAlerts
+namespace TwitterAlerts
+module Main =
 
-// Actors
-open Akka
-open Akka.Actor
-open Akka.FSharp
-open System
+    // Actors
+    open Akka
+    open Akka.Actor
+    open Akka.FSharp
+    open System
 
-let schedule (system: ActorSystem) actor message =
-    system
-        .Scheduler
-        .ScheduleTellRepeatedly(TimeSpan.FromSeconds(0.0), TimeSpan.FromSeconds(5.0), actor, message);
+    open FSharp.Data.Toolbox.Twitter
 
-type TwitterFeed = TwitterFeed of string
-
-type ManagerMessages = 
-    | WatchFeed of TwitterFeed
-
-let manager system watchers notifier =
-    spawn system "manager"
-        (fun mailbox ->
-            let rec loop() = actor {
-                let! message = mailbox.Receive()
-                
-                match message with
-                | WatchFeed(twitterFeed) -> 
-                    // Do validation
-                    watchers <! message
-
-                return! loop()
-            }
-            loop())
-
-type FeedMessages = 
-    | PollFeed
-
-let feed parent feed =
-    spawn parent "feed"
-        (fun mailbox ->
-
-            schedule mailbox.Context.System mailbox.Self PollFeed
-
-            let rec loop() = actor {
-                let! message = mailbox.Receive()
-                
-                match message with
-                | PollFeed ->
-                    // Call out to twitter
-                    // Send results back to parent
-                    ()
-
-                return! loop()
-            }
-            loop())
-
-let watchers system =
-    spawn system "watchers"
-        (fun mailbox ->
-            let rec loop() = actor {
-                let! message = mailbox.Receive()
-                
-                match message with
-                | WatchFeed(twitterFeed) ->
-                    // Do validation
-                    feed mailbox twitterFeed |> ignore
-
-                return! loop()
-            }
-            loop())
-
-let notifier system =
-    spawn system "notifier"
-        (fun mailbox ->
-            let rec loop() = actor {
-                let! message = mailbox.Receive()
-                
-                //
-
-                return! loop()
-            }
-            loop())
-
-// Twitter
-
-open FSharp.Data.Toolbox.Twitter
-
-let key = "mKQL29XNemjQbLlQ8t0pBg"
-let secret = "T27HLDve1lumQykBUgYAbcEkbDrjBe6gwbu0gqi4saM"
+    let key = "mKQL29XNemjQbLlQ8t0pBg"
+    let secret = "T27HLDve1lumQykBUgYAbcEkbDrjBe6gwbu0gqi4saM"
 
 
+    let schedule (system: ActorSystem) actor message =
+        system
+            .Scheduler
+            .ScheduleTellRepeatedly(TimeSpan.FromSeconds(0.0), TimeSpan.FromSeconds(5.0), actor, message);
 
-// Windows Notifications
+    type TwitterFeed = TwitterFeed of string
 
-[<EntryPoint>]
-let main argv =
-    
-    use system = System.create "my-system" (Configuration.load())
+    type WatcherMessages = 
+        | WatchFeed of TwitterFeed
 
-    let twitter = Twitter.AuthenticateAppOnly(key, secret)
+    type NewTweet = 
+        {
+            User: string;
+            Text: string;
+            Feed: TwitterFeed;
+        }
 
-    let fsharpTweets = twitter.Search.Tweets("#fsharp", count=100)
+    (*
+    let manager system watchers notifier =
+        spawn system "manager"
+            (fun mailbox ->
+                let rec loop() = actor {
+                    let! message = mailbox.Receive()
+                    
+                    match message with
+                    | WatchFeed(twitterFeed) -> 
+                        // Do validation
+                        watchers <! message
 
-    for status in fsharpTweets.Statuses do
-        printfn "@%s: %s" status.User.ScreenName status.Text
+                    return! loop()
+                }
+                loop())
+    *)
 
+    type FeedMessages = 
+        | PollFeed
 
-    0 // return an integer exit code
+    let feed parent notifier feed =
+        spawn parent "feed"
+            (fun mailbox ->
+
+                schedule mailbox.Context.System mailbox.Self PollFeed
+                let twitter = Twitter.AuthenticateAppOnly(key, secret)
+
+                let rec loop state = actor {
+                    let! message = mailbox.Receive()
+
+                    match message with
+                    | PollFeed ->
+                        let (TwitterFeed feedName) = feed
+                        let latestTweets = twitter.Search.Tweets("#fsharp", count = 100)
+
+                        for status in latestTweets.Statuses do
+                            notifier <! { Feed = feed; User = "???"; Text = status.Text }
+
+                    return! loop state
+                }
+                loop [])
+
+    let watchers system notifier =
+        spawn system "watchers"
+            (fun mailbox ->
+                let rec loop() = actor {
+                    let! message = mailbox.Receive()
+
+                    match message with
+                    | WatchFeed(twitterFeed) ->
+                        feed mailbox notifier twitterFeed |> ignore
+
+                    return! loop()
+                }
+                loop())
+
+    let notifier system =
+        spawn system "notifier"
+            (fun mailbox ->
+
+                let notifier = WindowsNotifications.NotificationManager("TwitterAlerts")
+
+                let rec loop() = actor {
+                    let! (message:NewTweet) = mailbox.Receive()
+
+                    let (TwitterFeed feedName) = message.Feed
+                    notifier.Toast(sprintf "New Tweet: %s" feedName, message.Text, message.User)
+
+                    return! loop()
+                }
+                loop())
+
+    [<EntryPoint>]
+    let main argv =
+
+        use system = System.create "my-system" (Configuration.load())
+
+        let notifier = notifier system
+        let watcher = watchers system notifier
+
+        watcher <! WatchFeed(TwitterFeed "Trump")
+
+        system.WhenTerminated
+            |> Async.AwaitTask
+            |> Async.Start
+
+        0 // return an integer exit code
